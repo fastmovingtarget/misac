9dac1cb0bbe46610b1bc615319886fb631cda690
Target <fastmovingtarget220689@gmail.com>
2024-12-22 13:29:01 +0000

    Note press handler now registers octave

--- misac/src/MisacPage.jsx
+++ misac/src/MisacPage.jsx
@@ -12,23 +12,23 @@ function MisacPage() {
 
   const {currentNoteSet, setCurrentNoteSet, keys, key, generateNewNoteSet, octaves, setHighScore, highScore} = useOptions()
   
-    const notePressHandler = (notePressed) => {
+    const notePressHandler = (notePressed, octaveNumber) => {
         const currentNoteSetIndex = currentNoteSet.findIndex((element) => element.state !== "successful");//find the index of the current incorrect or unused note in the current note set
         const currentNote = currentNoteSet[currentNoteSetIndex];
 
-        if(notePressed === keys[key][currentNote.noteKeyIndex]){//if the current unsuccessful note's index in the current key is equal to the pressed note
+        if(notePressed === keys[key][currentNote.noteKeyIndex] && octaveNumber === currentNote.octave){//if the current unsuccessful note's index in the current key is equal to the pressed note
             if(currentNoteSetIndex === currentNoteSet.length - 1){//and it's the last note in the set
-            setCurrentNoteSet(generateNewNoteSet())//regenerate the set
-        }
-        else{
-            let newNoteSet = [...currentNoteSet];
-            newNoteSet[currentNoteSetIndex].state = "successful"//set the state to success if the note isn't the last note in the set
-            setCurrentNoteSet(newNoteSet)
-        }
-        const newScore = score + 1000;
-        setScore(newScore);
-        if(newScore > highScore)
-            setHighScore(newScore)
+              setCurrentNoteSet(generateNewNoteSet())//regenerate the set
+            }
+            else{
+                let newNoteSet = [...currentNoteSet];
+                newNoteSet[currentNoteSetIndex].state = "successful"//set the state to success if the note isn't the last note in the set
+                setCurrentNoteSet(newNoteSet)
+            }
+            const newScore = score + 1000;
+            setScore(newScore);
+            if(newScore > highScore)
+                setHighScore(newScore)
         }
         else{//if the wrong note is pressed
             let newNoteSet = [...currentNoteSet];
